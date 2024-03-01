b171416e97d1fbd7c2fcb61e59eca380ca04d8ed
skitamur <skitamur@redhat.com>
2024-03-01 12:11:04 +0900

    fix

--- terraform-skeleton/provider.tf
+++ terraform-skeleton/provider.tf
@@ -6,6 +6,6 @@ terraform {
   backend "s3" {
     bucket = "skitamura-terraform-tfstate"
     key    = "tesk-eks-2/terraform.tfstate"
-    region = "ap-northeast-1"
+    region = "${{ values.aws_region }}"
   }
 }
